fe0b378691ef16d16ed481f8d373fae16d3ca2d2
Clay Mangum <clay@trinitasaccounting.com>
2024-02-17 06:27:10 -0500

    mostly complete

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,32 @@
 import React from "react";
+import Links from "./Links";
+import user from "../data/user";
 
-function About() {
+
+function IfBioPassed({ props }) {
+  if (!props.bio) {
+    return null;
+  }
+  else {
+    return (
+      <p>{props.bio}</p>
+    )
+  };
+}
+
+
+
+
+function About(props) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      <IfBioPassed({props} )/>
+      {/* <p>{props.bio ? props.bio : ""}</p> */}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       {/* add your <Links /> component here */}
+      <Links github={user.links.github} linkedin={user.links.linkedin} />
+
     </div>
   );
 }
---
+++ src/components/Links Component/Links.component.jsx
@@ -0,0 +1,22 @@
+// import React from "react";
+
+
+// function Links(props) {
+//     return (
+//         <div>
+//             <h1>Links</h1>
+
+//             <a href={props.github}>{props.github}</a>
+
+//             <a href={props.linkedin}>{props.linkedin}</a>
+
+
+//         </div>
+//         // <a href="https://github.com/liza">asdf</a>
+//         // <div>
+//         // </div>
+//         // <a href={props.links}>
+//     );
+// }
+
+// export default Links;
